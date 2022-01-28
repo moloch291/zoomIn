dfe38d5358fb932e5b625225c7eb0ac92e99b654
moloch291 <danielkomjati1@gmail.com>
2022-01-28 17:29:36 +0100

    fix output errors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"bufio"
 	"fmt"
 	"os"
@@ -8,8 +9,7 @@ import (
 )
 
 func main() {
-	fileName := os.Args[1]
-	stringToSearchFor := os.Args[2]
+	fileName, stringToSearchFor := os.Args[1], os.Args[2]
 	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
@@ -21,28 +21,27 @@ func main() {
 }
 
 func reader(file *os.File, stringToSearchFor string) string {
-	output := ""
 	scanner := bufio.NewScanner(file)
+	lineNumbers := ""
 	lineNumber := 1
 	for scanner.Scan() {
 		if strings.Contains(scanner.Text(), stringToSearchFor) {
-			output += string(rune(lineNumber)) + ", "
+			lineNumbers += strconv.Itoa(lineNumber) + ", "
 		}
 		lineNumber++
-
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	return output
+	return lineNumbers
 }
 
 func printResults(output string, stringToSearchFor string, fileName string) {
 	fmt.Println("\nSearch done! Results:")
 	if output != "" {
-		result := "'" + stringToSearchFor + "'" + "is presented in line(s) " + output[:len(output)-2] + " in" + fileName
-		fmt.Println(result)
+		fmt.Println(
+			"\n'" + stringToSearchFor + "' " + "is presented in line(s) " + output[:len(output) - 2] + " in " + fileName)
 	} else {
 		fmt.Println("The file doesn't contain", "'", stringToSearchFor, "'...")
 	}
