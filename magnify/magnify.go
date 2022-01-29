dd75b0ea2a174eeb9cb4ca39a1e4b4590f078549
moloch291 <danielkomjati1@gmail.com>
2022-01-29 22:24:36 +0100

    introduce first subfunction

---
+++ magnify/magnify.go
@@ -0,0 +1,54 @@
+package magnify
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func App() {
+	fileName, stringToSearchFor := os.Args[2], os.Args[3]
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+		deliverErrorMessage()
+		os.Exit(0)
+	}
+	defer file.Close()
+	output := getOccurrences(file, stringToSearchFor)
+	printResults(output, stringToSearchFor, fileName)
+}
+
+func deliverErrorMessage() {
+	fmt.Println("Unable to search... :(")
+}
+
+func getOccurrences(file *os.File, stringToSearchFor string) string {
+	scanner := bufio.NewScanner(file)
+	lineNumbers := ""
+	lineNumber := 1
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), stringToSearchFor) {
+			lineNumbers += strconv.Itoa(lineNumber) + ", "
+		}
+		lineNumber++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		deliverErrorMessage()
+		os.Exit(0)
+	}
+	return lineNumbers
+}
+
+func printResults(output string, stringToSearchFor string, fileName string) {
+	fmt.Println("\nSearch done!\n" + "'" + stringToSearchFor + "' " + "is presented in line(s):\n")
+	if output != "" {
+		fmt.Println(output[:len(output) - 2] + "\n\nin " + fileName)
+	} else {
+		fmt.Println("The file doesn't contain", "'", stringToSearchFor, "'...")
+	}
+}
+
--- main.go
+++ main.go
@@ -1,61 +1,16 @@
 package main
 
 import (
-	"bufio"
+	"zoomIn/magnify"
+
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	if len(os.Args) == 3 {
-		app()
+	if len(os.Args) == 4 && os.Args[1] == "--magnify" {
+		magnify.App()
 	} else {
 		fmt.Println("\nArgs missing or too much!\nUsage: 1st arg = fileName, 2nd arg = stringToSearch")
-	}
-}
-
-func app() {
-	fileName, stringToSearchFor := os.Args[1], os.Args[2]
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println(err)
-		deliverErrorMessage()
-		os.Exit(0)
-	}
-	defer file.Close()
-	output := getOccurrences(file, stringToSearchFor)
-	printResults(output, stringToSearchFor, fileName)
-}
-
-func deliverErrorMessage() {
-	fmt.Println("Unable to search... :(")
-}
-
-func getOccurrences(file *os.File, stringToSearchFor string) string {
-	scanner := bufio.NewScanner(file)
-	lineNumbers := ""
-	lineNumber := 1
-	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), stringToSearchFor) {
-			lineNumbers += strconv.Itoa(lineNumber) + ", "
-		}
-		lineNumber++
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-		deliverErrorMessage()
-		os.Exit(0)
-	}
-	return lineNumbers
-}
-
-func printResults(output string, stringToSearchFor string, fileName string) {
-	fmt.Println("\nSearch done!\n" + "'" + stringToSearchFor + "' " + "is presented in line(s):\n")
-	if output != "" {
-		fmt.Println(output[:len(output) - 2] + "\n\nin " + fileName)
-	} else {
-		fmt.Println("The file doesn't contain", "'", stringToSearchFor, "'...")
 	}
 }
